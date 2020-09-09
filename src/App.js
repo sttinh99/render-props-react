7858b03790f2a6452ca7c538d1df4d63c6305065
Quang Tịnh Trương <58225578+sttinh99@users.noreply.github.com>
2020-09-09 22:22:58 +0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -3,12 +3,28 @@ import "./styles.css";
 
 import Counter from "./components/Counter";
 import List from "./components/List";
+import NumberProvider from "./components/NumberProvider";
+import NumberContext from "./contexts/NumberContext";
+
+const data = ["A", "B", "C"];
+
+//provider consumer
 
 export default function App() {
   return (
-    <div className="App">
-      <List list={["A", "B", "C"]} render={(item) => <h2>{item}</h2>} />
-      <Counter>{(x) => x.count}</Counter>
-    </div>
+    <NumberProvider>
+      <div className="App">
+        <NumberContext.Consumer>
+          {({ number, updateNumber }) => (
+            <div>
+              <h2>{number}</h2>
+              <button onClick={updateNumber}>Update number</button>
+            </div>
+          )}
+        </NumberContext.Consumer>
+        <List list={data} render={(item) => <h2>{item}</h2>} />
+        <Counter>{(x) => x.count}</Counter>
+      </div>
+    </NumberProvider>
   );
 }
